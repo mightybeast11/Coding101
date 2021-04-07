b6e665292c3cedb6c330486b1003fb8e683bf554
mightybeast <u6123487@anu.edu.au>
2021-04-08 00:11:36 +0800

    BFS

---
+++ Leetcode/code/Q102Q107.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def levelOrderTraversal(root):
+    ret = [] # return value: list of list, each list inside is one level
+    # edge case
+    if root == None:
+        return ret
+    
+    d = deque() # deque for BFS
+    d.append(root)
+
+    while d: # ensure all nodes are covered
+        # each whhile loop covers one level
+        level_length = len(d) # key idea of BFS
+        level = []
+        for _ in range(level_length):
+            node = d.popleft()
+            level.append(node.val)
+            # expand next level
+            if node.left:
+                d.append(node.left)
+            if node.right:
+                d.append(node.right)
+        ret.append(level)
+    
+    print(ret)
+    return ret # Q102
+    # return list(reversed(ret)) # Q107
+
+if __name__=='__main__':
+    left = TreeNode(2, None, None)
+    right = TreeNode(3, None, None)
+    tree = TreeNode(1, left, right)
+    levelOrderTraversal(tree) # [[1], [2, 3]]
--- Python/Python小知识点.md
+++ Python/Python小知识点.md
@@ -12,3 +12,16 @@ for i, k, v in enumerate(dic.items()):
     print(i, k, v)
 ```
 
+
+
+# 创建deque
+
+创建新队列的参数必须是1个iterable。如果是一个node类型的instance，就会报错，只能先创建空deque：
+
+```python
+from collections import deque
+
+d = deque()
+d.append(root) # assume root is a Node class instance
+```
+
