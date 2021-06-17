fe47ba2a68ffaaf69e9b9b6a716566044ba175d7
mightybeast <u6123487@anu.edu.au>
2021-06-17 15:48:07 +0800

    :white_check_mark: Update Q65.

---
+++ Leetcode/code/Q65.py
@@ -0,0 +1,35 @@
+import re
+
+class Solution:
+    def isNumber(self, s):
+        regex = re.compile("^[+-]?(?:\\d+\\.?\\d*|\\.\\d+)(?:[Ee][+-]?\\d+)?$")
+        result = regex.match(s)
+        if result == None:
+            return False
+        return True
+
+if __name__ == '__main__':
+    sol = Solution()
+
+    # test 1
+    t1 = "0e"
+    print(sol.isNumber(t1))
+    
+    # test 2
+    t2 = ".1"
+    print(sol.isNumber(t2))
+
+
+    # Sequential decomposition
+    "^" # start of string
+
+    "[+-]?" # 0 or 1 sign, optional
+    "(?:)" # non-captureing group
+    "\\d+" # at least 1 digit. The start of int or float (with int part)
+    "\\.?" # 0 or 1 dot, optional. If 0 then int, if 1 then float (with int part)
+    "\\d*" # 0 or more digit. Can be later part of int, or just decimal part of float.
+
+    "()?" # 0 or 1 exponential part, optional
+    "[Ee]" # must have exactly 1, can be either E or e
+
+    "$" # start of string
