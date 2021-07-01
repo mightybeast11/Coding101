aabd2bf130f5ed6992e3a34ca9848fc7332a9e81
mightybeast <u6123487@anu.edu.au>
2021-07-02 04:57:57 +0800

    :white_check_mark: Update Q46.

--- Leetcode/code/Q46.py
+++ Leetcode/code/Q46.py
@@ -1,35 +1,18 @@
-# from itertools import permutations
-# def permute(nums):
-#     return list(map(list, permutations(nums, len(nums))))
-
-
-# 感谢liweiwei1419的题解:
 class Solution:
     def permute(self, nums):
-        def dfs(nums, size, depth, path, used, res):
-            # base case
-            if depth == size:
-                res.append(path[:]) # slicing to make a copy of path variable
+        def backtrack(depth):
+            if depth == n:
+                permutations.append(nums[:])
                 return
+            for i in range(depth, n):
+                nums[depth], nums[i] = nums[i], nums[depth]
+                backtrack(depth + 1)
+                nums[i], nums[depth] = nums[depth], nums[i]
 
-            for i in range(size):
-                if not used[i]:
-                    used[i] = True
-                    path.append(nums[i])
-
-                    dfs(nums, size, depth + 1, path, used, res) # recursion
-
-                    used[i] = False # 回溯
-                    path.pop() # 回溯
-
-        size = len(nums)
-        if len(nums) == 0:
-            return []
-
-        used = [False for _ in range(size)]
-        res = []
-        dfs(nums, size, 0, [], used, res)
-        return res
+        n = len(nums)
+        permutations = []
+        backtrack(0)
+        return permutations
 
 
 if __name__ == '__main__':
